538337337277b7ba8e1f4e91d49eab6a79e0aaa9
Fabio <fabio.fazio@villaggio.org>
2016-11-23 10:02:46 +0100

    Adattata pagina di esempio connessione PDO con entrambi gli esercizi

--- pdo_example.php
+++ pdo_example.php
@@ -16,18 +16,33 @@ $connessione =
     die('Connection failed: ' . $e->getMessage());
 }
 $connessione->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
-//    PDO::FETCH_NUM);
+//    PDO::FETCH_BOTH);
     PDO::FETCH_ASSOC);
 $result = $connessione->query("SELECT * FROM student");
-//echo json_encode($result->fetchAll());
+
+echo "<h3>Esempio di semplice query PDO</h3>";
+
+foreach($result as $riga){
+    foreach ($riga as $key => $value){
+        echo "$key : $value<br>";
+    }
+}
+
+echo "<hr><h3>Esempio di query con prepared statement filtrando per due valori in WHERE</h3>";
 
 // Eseguire una query filtrando per ID = 1
 // pescato da una variabile e appggiandosi
 // ad prepared statement
 
-$result = $connessione->prepare("SELECT * FROM student WHERE id = :id LIMIT 1");
-$id = 1;
+$result = $connessione->prepare("SELECT * FROM student WHERE age > :age AND email LIKE :email");
+$age = 50;
+$email = "%@usa%";
 $result->execute(array(
-	":id" => $id
+	":age" => $age,
+        ":email" => $email,
 ));
-echo json_encode($result->fetch());
+foreach($result as $riga){
+    foreach ($riga as $key => $value){
+        echo "$key : $value<br>";
+    }
+}
